ccf05b88820041a380de17cee3c406ca97ae1d23
Alan McConnell <alanmc@fb.com>
2014-11-11 18:22:12 -0800

    Add ChatViewController

---
+++ tinder/ChatViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChatViewController.h
+//  tinder
+//
+//  Created by Alan McConnell on 11/11/14.
+//  Copyright (c) 2014 Foo Bar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChatViewController : UIViewController
+
+@end
