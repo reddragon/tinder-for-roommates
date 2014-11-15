b68e58253be5ddabc7c1f9f3d85c0784438e465f
Alan McConnell <alanmc@fb.com>
2014-11-15 13:28:25 -0800

    Nav Bar, Pull to Refresh Chat Index, and Bug Fixes

---
+++ tinder/ViewType.h
@@ -0,0 +1,13 @@
+//
+//  ViewType.h
+//  tinder
+//
+//  Created by Alan McConnell on 11/15/14.
+//  Copyright (c) 2014 Foo Bar. All rights reserved.
+//
+
+#ifndef tinder_ViewType_h
+#define tinder_ViewType_h
+
+
+#endif
